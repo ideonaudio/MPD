f0d7e7005a1f4740401e290d1fac5c2ad5ec2f95
Max Kellermann <max.kellermann@gmail.com>
2026-08-12 07:58:43 +0200

    lib/nfs/Connection: explicitly invoke rpc_disconnect() in destructor
    Workaround for libnfs memory leak bug.

--- src/lib/nfs/Connection.cxx
+++ src/lib/nfs/Connection.cxx
@@ -10,6 +10,7 @@
 
 extern "C" {
 #include <nfsc/libnfs.h>
+#include <nfsc/libnfs-raw.h> // for nfs_get_rpc_context()
 }
 
 #include <utility>
@@ -240,6 +241,12 @@ NfsConnection::~NfsConnection() noexcept
 	   MountCallback() knows the mount call is being canceled;
 	   checking status==-EINTR would be ambiguous */
 	mount_timeout_event.Cancel();
+
+        /* cancel all pending async calls (e.g. nfs_mount_async();
+	   unfortunately, this is not implied by
+	   nfs_destroy_context(), and omitting the callbacks will leak
+	   memory */
+        rpc_disconnect(nfs_get_rpc_context(context), "Shutting down");
 
 	nfs_destroy_context(context);
 }
