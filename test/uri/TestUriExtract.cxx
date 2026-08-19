793eb1219e67659d0bb61737544c664ff821538a
Max Kellermann <max.kellermann@gmail.com>
2026-08-19 22:31:04 +0200

    test/uri/TestUriExtract: add UriPathQueryFragment() test

--- test/uri/TestUriExtract.cxx
+++ test/uri/TestUriExtract.cxx
@@ -8,6 +8,35 @@
 
 using std::string_view_literals::operator""sv;
 
+static constexpr struct UriTests {
+	const char *uri;
+	const char *host_and_port;
+	const char *path;
+	const char *query_string;
+} uri_tests[] = {
+	{ "http://foo/bar", "foo", "/bar", nullptr },
+	{ "https://foo/bar", "foo", "/bar", nullptr },
+	{ "http://foo:8080/bar", "foo:8080", "/bar", nullptr },
+	{ "http://foo", "foo", nullptr, nullptr },
+	{ "http://foo/bar?a=b", "foo", "/bar?a=b", "a=b" },
+	{ "whatever-scheme://foo/bar?a=b", "foo", "/bar?a=b", "a=b" },
+	{ "//foo/bar", "foo", "/bar", nullptr },
+	{ "//foo", "foo", nullptr, nullptr },
+	{ "/bar?a=b", nullptr, "/bar?a=b", "a=b" },
+	{ "bar?a=b", nullptr, "bar?a=b", "a=b" },
+};
+
+TEST(UriExtractTest, Path)
+{
+	for (auto i : uri_tests) {
+		auto result = UriPathQueryFragment(i.uri);
+		if (i.path == nullptr)
+			ASSERT_EQ(result.data(), nullptr);
+		else
+			ASSERT_EQ(result, i.path);
+	}
+}
+
 TEST(UriExtract, Suffix)
 {
 	EXPECT_EQ((const char *)nullptr, uri_get_suffix("/foo/bar").data());
